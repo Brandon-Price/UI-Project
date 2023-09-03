d050237f0171ac5e3ef3def219970186a1363be0
Brandon-Price <brandon.samuel.price@gmail.com>
2023-09-03 13:17:35 -0500

    Edited the product schema for Organic fruits

--- backend/models/Products.js
+++ backend/models/Products.js
@@ -5,11 +5,9 @@ const ProductSchema = new mongoose.Schema(
         title:{type: String, required:true, unique:true},
         desc:{type: String, required:true},
         img:{type: String, required: true},
-        categories:{type: Array},
-        size:{type: Array},
-        colors:{type: Array},
         price:{type: Number, required:true},
-        availability: {type:Boolean, default: true}
+        isOrganic:{type:String, required:true},
+        availability:{type:Boolean, default: true}
     },
     {timestamps: true});
 
