babcc13586c4df36cc7849b1c95603fee7cd7fee
Morgan D <124601263+mDean1@users.noreply.github.com>
2023-11-10 09:31:03 -0600

    Finished About page

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -2,18 +2,95 @@ import React from 'react';
 import styled from 'styled-components';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
+import { Container, ImageContainer, InfoContainer, Title, Description, Header, RowHeader, Row, ThumbnailContainer, RightDescription, LeftDescription, Image, Biography} from "../styles/About.styles";
 
-const Container = styled.div`
-    margin-top: 61px;
-`
 
 const About = () => {
     const page = 'about';
+    const pageName = 'About Us';
+    const imgUrl = "https://dailymedia.case.edu/wp-content/uploads/2018/09/17144905/apple-tree.jpg"
 
     return(
         <Container>
             <Navbar page={page}/>
-                About Page
+                <ImageContainer style={{ backgroundImage: `url(${imgUrl})` }}>
+                    <InfoContainer>
+                        <Title style={{ fontSize: "150px" }}>{pageName}</Title>
+                    </InfoContainer>
+                </ImageContainer>
+                <Description>
+                        At <b>Fruits R US</b>, we're on a mission to bring the orchard to your doorstep, one delicious fruit at a time. 
+                        <br></br>Our story is one of commitment to quality, ethical sourcing, and community empowerment.
+                </Description>               
+                <Container>
+                    <Header>
+                            Our History
+                        </Header>
+                        <Biography>
+                        Rooted in a shared love for fresh, ethically sourced fruits, our online fruit store sprouted into existence as a collective vision of change. 
+                        Established in 2005 by a group of friends passionate about making a positive impact, our journey has been marked by a commitment to ethical sourcing, sustainability, and community support. 
+                        From the idyllic valleys where our founders first shared their dreams to the global tables we now serve, each fruit on our virtual shelves carries the essence of responsible farming and a dedication to uplift local communities.
+                        Today, our online fruit store stands as a testament to the enduring values that have shaped our path, inviting customers to savor not just the flavors of our fruits but also the story of a conscious choice for a better, more sustainable world.
+                        </Biography>
+                    <Header>
+                        Our Values
+                    </Header>
+                    <RowHeader>
+                            Ethically and Locally Sourced
+                    </RowHeader>
+                    <Row>
+                        <ThumbnailContainer>
+                            <Image src="https://img.etimg.com/thumb/width-640,height-480,imgsize-758102,resizemode-75,msid-69025023/magazines/travel/wish-to-discover-a-new-culture-these-seasonal-fests-are-a-must-visit-for-you/durian-fruit.jpg" alt="Farmer"></Image>
+                        </ThumbnailContainer>
+                        <Container>
+                            <RightDescription>
+                            We take pride in the way we source our fruits.
+                            Our dedicated team works closely with local farmers across the country who share our commitment to sustainable and ethical farming practices.
+                            By supporting local growers, we not only ensure the freshest and most flavorful fruits but also help uplift communities and promote fair trade principles. 
+                            From the lush orchards to your doorstep, our fruits share generational stories with each bite.
+                            </RightDescription>
+                        </Container>
+                    </Row>
+
+                    <RowHeader>
+                            Guaranteed Freshness
+                    </RowHeader>
+
+                    <Row>
+                        <Container>
+                            <LeftDescription>
+                            Our promise to you is simple - freshness guaranteed! 
+                            From the moment the fruit is picked, to the careful packaging and swift delivery to your doorstep, we maintain the highest standards to ensure every bite is bursting with flavor and nutrients.
+                            We know that the best fruits are the ones that have ripened naturally, under the sun, and we're committed to delivering that natural goodness to your table.
+                            </LeftDescription>
+                        </Container>
+                        <ThumbnailContainer>
+                            <Image src="https://fruitezy.com.au/cdn/shop/products/Fruit-box-square_6131w.jpg?v=1610341013" alt="Farmer"></Image>
+                        </ThumbnailContainer>
+                    </Row>
+                    
+                    <RowHeader>
+                        Sowing the Seeds of Change
+                    </RowHeader>
+
+                    <Row>
+                        <ThumbnailContainer>
+                            <Image src="https://www.ollinfarms.com/family_web.jpg" alt="Farmer"></Image>
+                        </ThumbnailContainer>
+                        <Container>
+                            
+                            <RightDescription>
+                            More than just a fruit seller, we're on a mission to make a difference. We believe in giving back and supporting the communities that make our fruits possible. 
+                            With every purchase, you join us in this mission. 
+                            A portion of our proceeds goes directly to initiatives that uplift the communities of our dedicated farmers across the country. 
+                            We're proud to say that by choosing <b>Fruits R US</b>, you're leaving the world a little better than we found it, one fruit at a time.
+                            </RightDescription>
+                        </Container>
+                    </Row>
+                </Container>
+                <Description>
+                    <b>Taste the difference, make a difference.</b>
+                </Description>
             <Footer/>
         </Container>
     )
--- frontend/src/styles/About.styles.jsx
+++ frontend/src/styles/About.styles.jsx
@@ -1,1 +1,113 @@
-import styled from "styled-components";
+import styled, {keyframes} from "styled-components";
+import {COLORS} from "./Navbar.styles.jsx";
+
+export const Container = styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    text-align: center;
+    justify-content: top;
+    height: auto;
+    position: relative;
+    z-index: 1;
+    margin-top: 61px;
+`
+
+export const ImageContainer = styled.div`
+    max-height: 500px;
+    height: 500px;
+    min-width: 100%;
+    vertical-align: middle;
+    background-image: url("https://dailymedia.case.edu/wp-content/uploads/2018/09/17144905/apple-tree.jpg");
+    background-size: cover;
+    background-position: right bottom;
+    position: relative;
+    z-index: 1;
+`
+
+export const InfoContainer = styled.div`
+    width: fit-content;
+    margin-left: 10vw;
+    margin-top: 100px;
+    padding: 10px;
+    text-align: left;
+    overflow: hidden;
+    position: relative;
+    z-index: 1;
+`
+
+export const Title = styled.h1`
+    font-size: 70px;
+    color: ${COLORS.text};
+`
+
+export const Header = styled.h1`
+    font-size: 60px;
+    color: #f85c24;
+    padding-bottom:50px;
+    padding-top: 30px;
+`
+
+export const Description = styled.p`
+    padding: 30px 200px;
+    font-size: 20px;
+    font-weight: 500;
+    letter-spacing: 2px;
+`
+
+export const Biography = styled.p`
+    padding: 0px 200px;
+    margin-bottom:90px;
+    font-size: 24px;
+    font-weight: 400;
+    letter-spacing: 2px;
+`
+
+export const Row = styled.div`
+    display: flex;
+    text-align: center;
+    width: auto;
+    min-height: 200px;
+    padding-bottom: 70px;
+`
+
+export const RowHeader = styled.h1`
+    font-size: 30px;
+    font-weight: 600;
+    letter-spacing: 2px;
+    text-align: center; 
+    color: #2e2e2e;
+`
+
+export const ThumbnailContainer = styled.div`
+    width: 250px;
+    height: 250px;
+    object-fit: contain;
+    padding: 0px 60px;
+    overflow: hidden;
+`
+
+export const Image = styled.img`
+    height: 250px;
+    width: 250px;
+    object-fit: cover;
+    thumbnail-image: url("https://www.shutterstock.com/image-photo/happy-female-farmer-holding-box-260nw-2054595686.jpg");
+`
+
+export const RightDescription = styled.p`
+    padding-right: 300px;
+    padding-left: 20px;
+    font-size: 24px;
+    text-align: left;
+    font-weight: 400;
+    letter-spacing: 2px;
+`
+
+export const LeftDescription = styled.p`
+    padding-left: 300px;
+    font-size: 24px;
+    font-weight: 400;
+    text-align: right;
+    letter-spacing: 2px;
+`
